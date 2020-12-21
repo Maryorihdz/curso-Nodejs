78d97757a7b7a6a1052415ab331f02db5a6d0395
Maryorihdz <alejandrahdz17200@gmail.com>
2020-12-21 17:09:23 -0600

    Clase8

--- models/post.js
+++ models/post.js
@@ -1,14 +1,10 @@
-export  const  posts = [{
-    title: "Post 1",
-    time: "Dec 09 2020",
-    body: "Cupidatat anim consectetur enim enim nisi veniam mollit nisi amet deserunt quis dolor. Qui non id sint irure sit sint sit Lorem in. Ullamco ex aliquip proident fugiat ex sint. Exercitation et ad quis ullamco culpa mollit ex et. Non ullamco anim do ad eiusmod duis. Consectetur commodo magna labore aliquip officia deserunt dolore enim pariatur consequat do."
-},
-{ title: "Post 2",
-  time: "Dec 09 2020",
-  body: "Cupidatat anim consectetur enim enim nisi veniam mollit nisi amet deserunt quis dolor. Qui non id sint irure sit sint sit Lorem in. Ullamco ex aliquip proident fugiat ex sint. Exercitation et ad quis ullamco culpa mollit ex et. Non ullamco anim do ad eiusmod duis. Consectetur commodo magna labore aliquip officia deserunt dolore enim pariatur consequat do."
-},
-{ title: "Post 3",
-  time: "Dec 09 2020",
-  body: "Cupidatat anim consectetur enim enim nisi veniam mollit nisi amet deserunt quis dolor. Qui non id sint irure sit sint sit Lorem in. Ullamco ex aliquip proident fugiat ex sint. Exercitation et ad quis ullamco culpa mollit ex et. Non ullamco anim do ad eiusmod duis. Consectetur commodo magna labore aliquip officia deserunt dolore enim pariatur consequat do."
-}
-]
+import mongoose from 'mongoose'
+ const { Schema } = mongoose
+
+const postShema = new Schema({
+ title: String,
+ body: String
+});
+
+export const Post = mongoose.model('Post', postShema)
+
